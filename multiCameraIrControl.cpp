835f9598e75935fc64512c989f5e6a385b3d3850
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-05-01 22:35:03 +0800

    add(multiCameraIrControl.cpp)

--- multiCameraIrControl.cpp
+++ multiCameraIrControl.cpp
@@ -3,3 +3,4 @@
  * Name.......:  cameraIrControl Library
  * Description:  A powerful Library to control easy various cameras via IR. Please check the project page and leave a comment.
  * Author.....:  Sebastian Setz
+ * Version....:  1.9
