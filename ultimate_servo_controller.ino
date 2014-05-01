97ef953d1a7307eb34871542496e9147a58e1c2f
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-05-01 20:13:56 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -1,3 +1,4 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 // UNO_20_Servos_Controller.ino - High definition (15 bit), low jitter, 20 servo software for Atmega328P and Arduino UNO. Version 1.
+// Jitter is typical 200-400 ns. 32000 steps resolution for 0-180 degrees. In 18 servos mode it can receive serial servo-move commands.
