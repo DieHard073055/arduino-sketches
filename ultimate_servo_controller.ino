8bc0235978c003002962e2b994c677d58b72a3f8
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-05-05 21:35:02 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -8,3 +8,4 @@
 // 
 // This software is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
