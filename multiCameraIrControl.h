6f53ab369371da17d95b906e9e7121fc4adfd4c0
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-05-15 19:10:50 +0800

    add(multiCameraIrControl.h)

--- multiCameraIrControl.h
+++ multiCameraIrControl.h
@@ -20,3 +20,4 @@
  *               2011-12-05 V1.6 - adding Olympus zoom, thank you again Steve! Refresh keywords.txt; Arduino 1.0 compatible
  *               2011-12-07 V1.7 - repairing Canon function - thanks to Viktor
  *               2013-02-11 V1.8 - adding toggleFocus for Pentax - thanks to Marcel Normann
+ * 								   adding toggleVideo for Sony - thanks to InfinitR
