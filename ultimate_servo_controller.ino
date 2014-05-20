b4b8b11d4ffbde6e3fb4c07ef51afbee0cda089b
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-05-20 22:00:36 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -26,3 +26,4 @@
 //                              !!!!!!!!!!!!!!!!!!!
 // How it works:
 // 18 or 20 pins are used for the servos. These pins are all pre configured.
+// All 18/20 servos are always active and updated. To change the servo position, change the
