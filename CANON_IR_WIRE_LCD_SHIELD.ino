d21074cba1835a515ca1ea93e16e2b1644bfc96d
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-05-20 20:14:53 +0800

    add(CANON_IR_WIRE_LCD_SHIELD.ino)

--- CANON_IR_WIRE_LCD_SHIELD.ino
+++ CANON_IR_WIRE_LCD_SHIELD.ino
@@ -52,3 +52,5 @@ pin 3 connected to ir led
           if(!sleeping){
 
             sleep_elapsed = millis() - sleep_start;
+LiquidCrystal Display(8, 9, 4, 5, 6, 7);
+            if(sleep_elapsed >= 5000){
