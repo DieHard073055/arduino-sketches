6c5ef82c1e7374e797c4f075751f435b572a97b7
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-06-02 21:48:54 +0800

    add(LCD_SHIELD_TUTORIAL.ino)

--- LCD_SHIELD_TUTORIAL.ino
+++ LCD_SHIELD_TUTORIAL.ino
@@ -33,3 +33,4 @@ int read_LCD_buttons()
  if (adc_key_in < 250)  return btnUP; 
  if (adc_key_in < 450)  return btnDOWN; 
  if (adc_key_in < 650)  return btnLEFT; 
+ if (adc_key_in < 850)  return btnSELECT;  
