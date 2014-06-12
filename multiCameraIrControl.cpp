ba024945f2d20bff54927a37b44f3b7eed2bc13d
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-06-12 19:37:41 +0800

    add(multiCameraIrControl.cpp)

--- multiCameraIrControl.cpp
+++ multiCameraIrControl.cpp
@@ -42,3 +42,4 @@ void wait(unsigned int time){
 void high(unsigned int time, int freq, int pinLED){
   int pause = (1000/freq/2)-4;
   unsigned long start = micros();
+  while(micros()-start<=time){
