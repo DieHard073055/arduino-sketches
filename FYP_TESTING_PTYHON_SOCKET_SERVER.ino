85036e10b9984c92ded56b2d8c8c068020456ccc
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-06-30 19:16:24 +0800

    add(FYP_TESTING_PTYHON_SOCKET_SERVER.ino)

--- FYP_TESTING_PTYHON_SOCKET_SERVER.ino
+++ FYP_TESTING_PTYHON_SOCKET_SERVER.ino
@@ -64,3 +64,4 @@ void process_serial_commands(){
              SPRINTLN(command_header[1]);
            }else if(command_header[0] == SENSOR){
              //If sensor reading requested
+             //Return the sensor reading
