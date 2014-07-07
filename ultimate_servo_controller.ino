40495686a65be0288c13f85b99f58ec8caca327c
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-07-07 21:15:54 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -74,3 +74,4 @@
 // 18 or 20 channels mode:
 // #define HDServoMode 18           - This will set 18 channels mode so you can use serial in and out. Serial command interpreter is activated.
 // #define HDServoMode 20           - This will set 20 channels mode, and you can not use serial. 
+//                                    A demo will run in the loop() routine . Serial command interpreter is not active.
