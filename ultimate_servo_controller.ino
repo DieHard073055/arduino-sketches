16520072fd26204f20c78138749dcf47602e8268
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-07-08 18:32:58 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -80,3 +80,4 @@
 //                                    the one that takes the longest time will be used. You can also change the values in the 
 //                                    ServoPW[] array directly, but take care not to go under/over 8320/39680.
 //
+// #deefine SerialInterfaceSpeed 115200      - Serial interface Speed
