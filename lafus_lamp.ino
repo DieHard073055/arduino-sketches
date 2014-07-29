8e8c0860d88e54fb0049d9aaa84a64d68fc24898
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-07-30 01:59:06 +0800

    add(lafus_lamp.ino)

--- lafus_lamp.ino
+++ lafus_lamp.ino
@@ -113,3 +113,4 @@ void loop(){
 void setBrightness(int level){
     if(level < 8 && level > 0){
         digitalWrite(_LATCH, LOW);
+        shiftOut(_DATA, _CLCK, LSBFIRST, brightnessLevel[level]);
