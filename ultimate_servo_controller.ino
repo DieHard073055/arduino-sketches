b9943dfe720282d281a8e0da0f39166611195467
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-07-30 20:55:08 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -116,3 +116,4 @@ static int ServoGroupNbOfChannels = 0;
 static char SerialIn;
 static int SerialCommand = 0; //0= none, 1 = '#' and so on...
 static long SerialNumbers[10];
+static int SerialNumbersLength = 0;
