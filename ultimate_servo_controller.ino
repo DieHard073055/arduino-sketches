0d4d37db412b387ea619b28e2843a9ff4670e9c9
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-08-01 20:28:56 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -131,3 +131,4 @@ static char SerialCharToSend[50] = ".detratS slennahC 81 ovreSDH";
 static int SerialNbOfCharToSend = 0;  //0= none, 1 = [0], 2 = [1] and so on...
 
 void ServoMove(int Channel, long PulseHD, long SpeedHD, long Time)
+{
