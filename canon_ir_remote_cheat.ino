bdc42069fc408b9bba624dcfb7cfd6a0cb551459
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-08-22 17:16:00 +0800

    add(canon_ir_remote_cheat.ino)

--- canon_ir_remote_cheat.ino
+++ canon_ir_remote_cheat.ino
@@ -173,3 +173,4 @@ void setupSoftwareInterrupt(){
 
   TCNT1 = 34268;            // preload timer 65536-16MHz/256/2Hz
   TCCR1B |= (1 << CS10) ;   //prescale at clck/8
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
