09ae34303023d15e16d5c5a9ed3d2f012994b6cd
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-11-03 20:09:00 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -299,3 +299,4 @@ long CheckRange(long PulseHDValue)
         SerialPulseOffsetTempHD = ConvertSerialNumbers();
         SerialPulseHD = ServoPW[SerialChannel] - SerialPulseOffsetHD[SerialChannel] + SerialPulseOffsetTempHD;
         SerialTime = 10;
+        SerialPulseOffsetHD[SerialChannel] = SerialPulseOffsetTempHD;
