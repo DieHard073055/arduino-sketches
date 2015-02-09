4f9134c73ce238953a3e9e2fd221bfa19e83bbb6
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-02-09 23:05:41 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -415,3 +415,4 @@ ISR(TIMER2_COMPB_vect) // Interrupt routine for timer 2 compare A. Used for timi
   *OutPort1A &= ~OutBit1A;                           // Set pulse low to if not done already
   *OutPort1B &= ~OutBit1B;                           // Set pulse low to if not done already
   OutPort1A = OutPortTable[Timer2Toggle];            // Temp port for COMP1A
+  OutBit1A = OutBitTable[Timer2Toggle];              // Temp bitmask for COMP1A
