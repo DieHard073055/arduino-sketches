5ea20c1f448c43404dbb3e30066f1f72ddf8af00
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-02-11 23:43:24 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -425,3 +425,4 @@ ISR(TIMER2_COMPB_vect) // Interrupt routine for timer 2 compare A. Used for timi
   Timer2Toggle++;                                    // Next servo in line.
   if(Timer2Toggle==10)
   { 
+    Timer2Toggle = 0;                                // If next servo is grater than 9, start on 0 again.
