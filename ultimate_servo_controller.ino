908ad041527a449a838ea4df0d3bc516d7e4a0d8
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-03-05 19:08:57 +0800

    add(ultimate_servo_controller.ino)

--- ultimate_servo_controller.ino
+++ ultimate_servo_controller.ino
@@ -450,3 +450,4 @@ void ServoSetup()
   TIMSK2 = 6;                     // Enable the output compare A and B interrupt 
   OCR2A = 93;                     // Set counter A for about 500us before counter B below;
   OCR2B = 124;                    // Set counter B for about 2000us (20ms/10, where 20ms is 50Hz);
+
