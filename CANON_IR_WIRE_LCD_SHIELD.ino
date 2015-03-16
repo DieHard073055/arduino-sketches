c81b988ef619c9b680626dc0d5016da8557733ab
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-03-16 23:51:36 +0800

    add(CANON_IR_WIRE_LCD_SHIELD.ino)

--- CANON_IR_WIRE_LCD_SHIELD.ino
+++ CANON_IR_WIRE_LCD_SHIELD.ino
@@ -553,3 +553,4 @@ unsigned long elapsed = 0, pause_t = 0;
                  settings_value[setting]-=increment_value; 
                  if(settings_value[setting] < 0) settings_value[setting] = 0;
                }
+            }
