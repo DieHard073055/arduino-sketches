0cd33194131fbaff45012e5e31e3c5483bf40f49
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-04-01 20:43:26 +0800

    add(CANON_IR_WIRE_LCD_SHIELD.ino)

--- CANON_IR_WIRE_LCD_SHIELD.ino
+++ CANON_IR_WIRE_LCD_SHIELD.ino
@@ -586,3 +586,4 @@ unsigned long elapsed = 0, pause_t = 0;
             if(mode == 0){
                if(setting != 3){
                  if(increment_value < 10000){
+                   increment_value *= 10;
